58c0878ffc48b60a960d986319ad11e6c08c7dc0
nhat duong <dcnhat1862000@gmail.com>
2024-06-30 16:55:17 +0700

    Update font

--- src/App.js
+++ src/App.js
@@ -50,15 +50,8 @@ export default function App() {
 
   const options = {
     colors: ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b"],
-    enableTooltip: true,
-    deterministic: false,
-    fontFamily: "impact",
     fontSizes: [14, 60],
-    fontStyle: "normal",
-    fontWeight: "normal",
-    padding: 1,
     rotations: 3,
-    rotationAngles: [0, 90],
     scale: "sqrt",
     spiral: "archimedean",
     transitionDuration: 1000
